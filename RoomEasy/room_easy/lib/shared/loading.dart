b049a7b5693e3b10df3c4e9c5770796829c37c0b
kevin <kevindkim723@gmail.com>
2022-01-15 02:16:29 -0800

    More todos 1)Fix registration page to actually show the push notifications for shite logins

--- RoomEasy/room_easy/lib/shared/loading.dart
+++ RoomEasy/room_easy/lib/shared/loading.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("LOADING"),
-    );
+    return SpinKitChasingDots(color: Colors.teal,size: 50,);
   }
 }
