7f8a1b6b25cb3fbc8f9c0cf0029fbc442754b2a0
allyson-y <ayao20@holynames-sea.org>
2022-02-24 21:16:55 -0800

    color and formatting

--- RoomEasy/room_easy/lib/screens/home/swipe/swipe_card.dart
+++ RoomEasy/room_easy/lib/screens/home/swipe/swipe_card.dart
@@ -6,136 +6,170 @@ import 'package:flutter/material.dart';
 //THIS IS CURRENTLY JUST A SKELETON TEMPLATE
 class SwipeCard extends StatelessWidget {
   String name;
+
   SwipeCard(@required this.name);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffffefaf),
       body: Column(
         children: [
-          Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.deepOrange, Colors.pinkAccent])),
-              child: Container(
-                width: double.infinity,
-                height: 350.0,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          "https://i5.wal.co/asr/d90b3969-acb4-4ed1-ae23-2c505b7baf33.100242461d922915c2159e4b84e35366.jpeg?odnBg=ffffff&odnHeight=612&odnWidth=612",
-                        ),
-                        radius: 50.0,
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "Natasha",
-                        style: TextStyle(
-                          fontSize: 22.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Card(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 5.0),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.white,
-                        elevation: 5.0,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 22.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Posts",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "1200",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Followers",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "21.2K",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Follow",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "1200",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+          // Container(
+          // decoration: BoxDecoration(
+          //     gradient: LinearGradient(
+          //         begin: Alignment.topCenter,
+          //         end: Alignment.bottomCenter,
+          //         colors: [Colors.deepOrange, Colors.pinkAccent])),
+          // child: Container(
+          //   width: double.infinity,
+          //   height: 350.0,
+          //   child: Center(
+          //     child: Column(
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         CircleAvatar(
+          //           backgroundImage: NetworkImage(
+          //             "https://i5.wal.co/asr/d90b3969-acb4-4ed1-ae23-2c505b7baf33.100242461d922915c2159e4b84e35366.jpeg?odnBg=ffffff&odnHeight=612&odnWidth=612",
+          //           ),
+          //           radius: 50.0,
+          //         ),
+          //         SizedBox(
+          //           height: 10.0,
+          //         ),
+          //         Text(
+          //           "Natasha",
+          //           style: TextStyle(
+          //             fontSize: 22.0,
+          //             color: Colors.white,
+          //           ),
+          //         ),
+          //         SizedBox(
+          //           height: 10.0,
+          //         ),
+          //         Card(
+          //           margin: EdgeInsets.symmetric(
+          //               horizontal: 20.0, vertical: 5.0),
+          //           clipBehavior: Clip.antiAlias,
+          //           color: Colors.white,
+          //           elevation: 5.0,
+          //           child: Padding(
+          //             padding: const EdgeInsets.symmetric(
+          //                 horizontal: 8.0, vertical: 22.0),
+          //             child: Row(
+          //               children: [
+          //                 Expanded(
+          //                   child: Column(
+          //                     children: [
+          //                       Text(
+          //                         "Posts",
+          //                         style: TextStyle(
+          //                           color: Colors.redAccent,
+          //                           fontSize: 22.0,
+          //                           fontWeight: FontWeight.bold,
+          //                         ),
+          //                       ),
+          //                       SizedBox(
+          //                         height: 5.0,
+          //                       ),
+          //                       Text(
+          //                         "1200",
+          //                         style: TextStyle(
+          //                           fontSize: 20.0,
+          //                           color: Colors.pinkAccent,
+          //                         ),
+          //                       )
+          //                     ],
+          //                   ),
+          //                 ),
+          //                 Expanded(
+          //                   child: Column(
+          //                     children: [
+          //                       Text(
+          //                         "Followers",
+          //                         style: TextStyle(
+          //                           color: Colors.redAccent,
+          //                           fontSize: 22.0,
+          //                           fontWeight: FontWeight.bold,
+          //                         ),
+          //                       ),
+          //                       SizedBox(
+          //                         height: 5.0,
+          //                       ),
+          //                       Text(
+          //                         "21.2K",
+          //                         style: TextStyle(
+          //                           fontSize: 20.0,
+          //                           color: Colors.pinkAccent,
+          //                         ),
+          //                       )
+          //                     ],
+          //                   ),
+          //                 ),
+          //                 Expanded(
+          //                   child: Column(
+          //                     children: [
+          //                       Text(
+          //                         "Follow",
+          //                         style: TextStyle(
+          //                           color: Colors.redAccent,
+          //                           fontSize: 22.0,
+          //                           fontWeight: FontWeight.bold,
+          //                         ),
+          //                       ),
+          //                       SizedBox(
+          //                         height: 5.0,
+          //                       ),
+          //                       Text(
+          //                         "1200",
+          //                         style: TextStyle(
+          //                           fontSize: 20.0,
+          //                           color: Colors.pinkAccent,
+          //                         ),
+          //                       )
+          //                     ],
+          //                   ),
+          //                 ),
+          //               ],
+          //             ),
+          //           ),
+          //         )
+          //       ],
+          //     ),
+          //   ),
+          // )),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 10.0),
+                child: new Image.asset(
+                  'assets/profile.png',
+                  width: 200.0,
                 ),
-              )),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Natasha",
+                style: TextStyle(
+                  fontSize: 22.0,
+                  color: Color(0xff272324),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(3.0),
+                child: Center(
+                  child: Text('Hi! I am looking for a roommate.',
+                      style: TextStyle(fontSize: 15.0)),
+                ),
+              ),
+            ],
+          ),
           Container(
             child: Padding(
               padding:
@@ -169,11 +203,12 @@ class SwipeCard extends StatelessWidget {
             ),
           ),
           SizedBox(
-            height: 20.0,
+            height: 10.0,
           ),
           Container(
             width: 300.00,
             child: RaisedButton(
+                color: Color(0xffe24e3e),
                 onPressed: () {},
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(80.0)),
@@ -181,10 +216,7 @@ class SwipeCard extends StatelessWidget {
                 padding: EdgeInsets.all(0.0),
                 child: Ink(
                   decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft,
-                        colors: [Colors.pink, Colors.pinkAccent]),
+                    color: Color(0xffe24e3e),
                     borderRadius: BorderRadius.circular(30.0),
                   ),
                   child: Container(
@@ -192,7 +224,7 @@ class SwipeCard extends StatelessWidget {
                         BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
                     alignment: Alignment.center,
                     child: Text(
-                      "Contact me",
+                      "Message",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 26.0,
