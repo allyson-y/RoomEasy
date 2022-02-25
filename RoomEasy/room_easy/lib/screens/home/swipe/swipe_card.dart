57e33ba11258136cd8e930107e5ad6f47fac9d7b
Kevin <kevin12kim27@gmail.com>
2022-02-25 04:20:26 -0800

    another bug caught - Initially the matchlist is declared null. Bug was in swipe_card that tried calling contains() on a null list.

--- RoomEasy/room_easy/lib/screens/home/swipe/swipe_card.dart
+++ RoomEasy/room_easy/lib/screens/home/swipe/swipe_card.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:room_easy/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:room_easy/models/user.dart';
-import 'package:room_easy/screens/authenticate/authenticate.dart';
-import 'package:room_easy/screens/survey/survey.dart';
 import 'package:room_easy/services/auth.dart';
 import 'package:room_easy/services/database.dart';
 
@@ -22,7 +15,9 @@ import 'package:room_easy/services/database.dart';
 class SwipeCard extends StatefulWidget {
   bool inMatchList_ = false;
   RmEasyUser user_;
+
   SwipeCard(@required this.user_);
+
   @override
   _SwipeCardState createState() => _SwipeCardState();
 }
@@ -47,192 +42,201 @@ class _SwipeCardState extends State<SwipeCard> {
     final currUser = Provider.of<RmEasyUser>(context);
     final _auth = context.watch<AuthService>().auth;
     String buttonText = "";
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.deepOrange, Colors.pinkAccent])),
-              child: Container(
-                width: double.infinity,
-                height: 350.0,
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          "https://i5.wal.co/asr/d90b3969-acb4-4ed1-ae23-2c505b7baf33.100242461d922915c2159e4b84e35366.jpeg?odnBg=ffffff&odnHeight=612&odnWidth=612",
+    return (currUser == null)
+        ? SpinKitDoubleBounce(
+            color: Colors.pink,
+            size: 50,
+          )
+        : Scaffold(
+            body: Column(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [Colors.deepOrange, Colors.pinkAccent])),
+                    child: Container(
+                      width: double.infinity,
+                      height: 350.0,
+                      child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                "https://i5.wal.co/asr/d90b3969-acb4-4ed1-ae23-2c505b7baf33.100242461d922915c2159e4b84e35366.jpeg?odnBg=ffffff&odnHeight=612&odnWidth=612",
+                              ),
+                              radius: 50.0,
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              "SW4GM0N3YBAGS",
+                              style: TextStyle(
+                                fontSize: 22.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Card(
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 20.0, vertical: 5.0),
+                              clipBehavior: Clip.antiAlias,
+                              color: Colors.white,
+                              elevation: 5.0,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 22.0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Posts",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "1200",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.pinkAccent,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Followers",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "21.2K",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.pinkAccent,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "Follow",
+                                            style: TextStyle(
+                                              color: Colors.redAccent,
+                                              fontSize: 22.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 5.0,
+                                          ),
+                                          Text(
+                                            "1200",
+                                            style: TextStyle(
+                                              fontSize: 20.0,
+                                              color: Colors.pinkAccent,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
                         ),
-                        radius: 50.0,
                       ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        "SW4GM0N3YBAGS",
-                        style: TextStyle(
-                          fontSize: 22.0,
-                          color: Colors.white,
+                    )),
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 30.0, horizontal: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Info:",
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 28.0),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Card(
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 5.0),
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.white,
-                        elevation: 5.0,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0, vertical: 22.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Posts",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "1200",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Followers",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "21.2K",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      "Follow",
-                                      style: TextStyle(
-                                        color: Colors.redAccent,
-                                        fontSize: 22.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 5.0,
-                                    ),
-                                    Text(
-                                      "1200",
-                                      style: TextStyle(
-                                        fontSize: 20.0,
-                                        color: Colors.pinkAccent,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Text(
+                          widget.user_.name_,
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.black,
+                            letterSpacing: 2.0,
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ),
-              )),
-          Container(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Info:",
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    widget.user_.name_,
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.black,
-                      letterSpacing: 2.0,
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-              width: 300.00,
-              //Match Button!
-              child: ElevatedButton(
-                onPressed: () async {
-                  await DatabaseService().updateMatchList(
-                      AuthService().auth.currentUser.uid, widget.user_.uid_);
-                },
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    currUser.matchList_.contains(widget.user_.uid_)
-                        ? "already sent"
-                        : "match",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.w300),
-                  ),
                 ),
-              )),
-        ],
-      ),
-    );
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    width: 300.00,
+                    //Match Button!
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await DatabaseService().updateMatchList(
+                            AuthService().auth.currentUser.uid,
+                            widget.user_.uid_);
+                      },
+                      child: Container(
+                        constraints:
+                            BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                        alignment: Alignment.center,
+                        child: Text(
+                          (currUser.matchList_ == null ||
+                                  currUser.matchList_
+                                      .contains(widget.user_.uid_))
+                              ? "already sent"
+                              : "match",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          );
   }
 }
